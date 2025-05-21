3576b37ccb5a0753cd52b87e54d2a7c48e391804
Artem <no-name@gmail.com>
2025-05-21 09:02:47 +0300

    Update show.py

--- src/utils/show.py
+++ src/utils/show.py
@@ -1,8 +1,9 @@
+import os
 import json
 import tempfile
 from matplotlib import pyplot
-from pathlib import Path
 from typing import Optional
+from pathlib import Path
 
 def load_data(data_folder: Optional[Path] = None) -> list[dict]:
     if data_folder is None:
@@ -17,25 +18,28 @@ def load_data(data_folder: Optional[Path] = None) -> list[dict]:
     return result
 
 
-def main(save_to: Path):
+def generate_charts(save_to: Path):
     dataset = load_data()
-    hours = list(range(24))
+    save_to = save_to / Path("FairWatt")
+    os.mkdir(save_to)
 
     for id, consumption in enumerate(dataset):
         personal = consumption["rating"] <= 0.5
         pyplot.figure(figsize=(10, 6))
-        pyplot.plot(hours, consumption["consumption"], marker="o", linestyle="-", color="b" if personal else "r", label="кВт·ч")
+        pyplot.plot(range(24), consumption["consumption"], marker="o", linestyle="-", color="b" if personal else "r", label="кВт·ч")
 
-        pyplot.title(f"{'Personal' if personal else 'Commercial'} ({consumption['rating']})")
+        pyplot.title(f"{'Персональное' if personal else 'Коммерческое'} использование ({consumption['rating']})")
         pyplot.xlabel("Время")
         pyplot.ylabel("кВт·ч")
         pyplot.grid(True)
-        pyplot.xticks(hours)
+        pyplot.xticks(range(24))
 
         pyplot.savefig(save_to / Path(f"{id}.png"))
         pyplot.close()
     print(f"== Графики сохранены в '{save_to}' ==")
 
-main(Path(
-    tempfile.gettempdir()
-))
+generate_charts(
+    Path(
+        tempfile.gettempdir()
+    )
+)
