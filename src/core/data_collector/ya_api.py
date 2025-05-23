833cfcabcbc56500d49b7f17afe93d4a576813d4
Artem <no-name@gmail.com>
2025-05-23 21:49:02 +0300

    Refactor ya_api.py

--- src/core/data_collector/ya_api.py
+++ src/core/data_collector/ya_api.py
@@ -1,59 +1,51 @@
 import requests
 
-def cords_of_address(address: str, api_ya_geocode: str) -> str:
+def cords_of_address(address: str, api_ya_geocode: str) -> tuple[float, float]:
     """
     Поиск координат по адресу
     :param address: Полный адрес
     :param  api_ya_geocode: API ключ yandex геокодер - developer.tech.yandex.ru
 
-    :return str: Координаты или 0,0 при ошибке
+    :return tuple[float]: Координаты или 0,0 при ошибке
     """
-    base_url = "https://geocode-maps.yandex.ru/"
-    method = "v1"
+    base_url = "https://geocode-maps.yandex.ru/v1"
 
     response = requests.get(
         base_url +
-        method +
         f"?apikey={api_ya_geocode}" +
         f"&geocode={address}" +
         "&format=json"
     )
 
     if response.status_code != 200:
-        return "0,0" # TODO: Ошибка
+        return (0,0) # TODO: Ошибка
 
-    json = response.json()["response"]["GeoObjectCollection"]["featureMember"]
+    feature_member = response.json()["response"]["GeoObjectCollection"]["featureMember"]
 
-    if len(json) == 0:
-         return "0,0" # TODO: Ошибка
+    if not len(feature_member):
+         return (0,0) # TODO: Ошибка
 
-    return str(json[0]["GeoObject"]["Point"]["pos"]).replace(" ", ",")
+    x, y = feature_member[0]["GeoObject"]["Point"]["pos"].split()
+    return (float(x), float(y))
 
 
 def panorama_url_by_address(address: str, api_ya_geocode: str) -> str:
     """
-    Создание ссылки на панорамы яндекс карт по адресу
+    Генерерация ссылки на панорамы яндекс карт
     :param address: Полный адрес
     :param  api_ya_geocode: API ключ yandex геокодер - developer.tech.yandex.ru
 
-    :return str: Ссылка или yandex.ru при ошибке
+    :return str: Ссылка
     """
 
     pos = cords_of_address(address, api_ya_geocode)
-    if pos == "0,0": # TODO: Нормальная ошибка и её обработка
-        return "https://yandex.ru"
+    if pos == (0,0): # TODO: Ошибка и её обработка
+        return "https://yandex.ru/maps"
 
-    base_url = "https://yandex.ru/"
-    method = "maps"
+    base_url = "https://yandex.ru/maps"
 
     return (base_url +
-            method +
-            f"?l=stv,sta" +
-            f"&ll={pos}" +
-            "&panorama[direction]=0.000000,0.000000" +
+            "?panorama[direction]=0.000000,0.000000" +
             "&panorama[full]=true" +
-            f"&panorama[point]={pos}" +
-            "&panorama[span]=120.692570,60.000000" +
-            "&z=18"
-            )
-
+            f"&panorama[point]={pos[0]},{pos[1]}"
+    )
