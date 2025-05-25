8bed2988c78232c4e28005c4d9d345ecf23a7c11
xyz8778 <212346931+xyz8778@users.noreply.github.com>
2025-05-25 11:46:09 +0300

    Add CORS and statistics

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from sqlalchemy import text
 from uvicorn import run
 
 from src.api.routers import meters, employees, trips, data_collector, statistics, finance
 from src.config import config
-from src.db.session import init, dispose, session
+from src.db.session import init, dispose
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def main():
@@ -15,11 +15,20 @@ def main():
         description="🏔️ A project to analyze energy consumption by Elbrus team",
     )
 
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials=True,
+    )
+
     app.include_router(trips.router, prefix="/api/v1")
     app.include_router(data_collector.router, prefix="/api/v1")
     app.include_router(meters.router, prefix="/api/v1")
     app.include_router(employees.router, prefix="/api/v1")
     app.include_router(finance.router, prefix="/api/v1")
+    app.include_router(statistics.router, prefix="/api/v1")
 
     run(app, host=config.api_host, port=config.api_port, reload=False)
 
